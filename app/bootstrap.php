31dc92db32948306100c00455c135e1baeec74ee
ctopher-vargas <ctopher.vargas@gmail.com>
2018-10-31 10:52:58 -0700

    initial upload

---
+++ app/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+//autoload core libraries
+spl_autoload_register(function($className){
+	require_once 'libraries/' . $className . '.php'; 
+}); 
+// //load libraries
+// require_once 'libraries/Core.php'; 
+// require_once 'libraries/Controller.php'; 
+// require_once 'libraries/Database.php'; 
+//load config
+require_once 'config/config.php'; 
+
+?>
---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+	//DB Params
+	define('DB_HOST', 'localhost'); 
+	define('DB_USER', '_YOUR_USER_'); 
+	define('DB_PASS', '_YOUR_PASSWORD'); 
+	define('DB_NAME', '_YOUR_DBNAME'); 
+	//App Root
+	define('APPROOT', dirname(dirname(__FILE__))); 
+	//URL Root 
+	define('URLROOT', '__YOUR__URL__); 
+	//site name 
+	define('SITENAME', '__YOUR_SITENAME__'); 
+?>
